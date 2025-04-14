3f351fe756d8b8365efe2d3c2956dea3981a1ade
HermanSkop <armaton111@gmail.com>
2025-04-14 12:05:54 +0200

    Add unit tests for authentication and user service

--- src/test/java/org/example/liquoriceauthenticationserver/LiquoriceAuthenticationServerApplicationTests.java
+++ src/test/java/org/example/liquoriceauthenticationserver/LiquoriceAuthenticationServerApplicationTests.java
@@ -2,8 +2,10 @@ package org.example.liquoriceauthenticationserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LiquoriceAuthenticationServerApplicationTests {
 
     @Test
---
+++ src/test/java/org/example/liquoriceauthenticationserver/config/TestConfig.java
@@ -0,0 +1,27 @@
+package org.example.liquoriceauthenticationserver.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.mockito.Mockito;
+
+@TestConfiguration
+public class TestConfig {
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return Mockito.mock(MongoTemplate.class);
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
+    }
+}
